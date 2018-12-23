a40f58342ffbb9c43aaf5366cd6bfd3387d2a0e5
Harvey56 <hevel56@gmail.com>
2018-12-23 23:53:50 +1100

    Updated server - Business search functional

--- server/src/index.js
+++ server/src/index.js
@@ -1,42 +1,27 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
-
-
-
-// 1
-const typeDefs = gql`
-	type Query {
-		hello: String
-	}
-`;
-
-const resolvers = {
-	Query: {
-		hello: () => 'Hello world!'
-	}
-};
-
-// const server = new ApolloServer({
-//   // schema definition
-//   typeDefs: './src/schema.graphql',
-//   // resolvers
-//   resolvers: './src/resolvers',
-// });
-
-// server.applyMiddleware({ app });
-
-// app.listen({ port: 4000 }, () =>
-//   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-// )
+const cors = require('cors');
+const typeDefs = require('./schema.graphql');
+const resolvers = require('./resolvers');
+const YelpAPI = require('../datasources/businesses');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   playground: true,
-  introspection: true
+  introspection: true,
+  dataSources: () => ({
+	  YelpAPI: new YelpAPI()
+  }),
+  context: () => {
+    return {
+      token: "Bearer NXsuVfuXjTiJmlSs1dHn92xnXFpo3G1KaJE0lzQoAOuEMbNyhXkyeWEWoril1xWZU1sMSJQgx1ZAJ0drAup9Sa9FNf5eJCKiWb7_t1eWRhSlkaVuWFXGK2cUjSgaXHYx",
+    };
+  },
 })
 
 const app = express();
+app.use(cors());
 
 server.applyMiddleware({ app });
 
