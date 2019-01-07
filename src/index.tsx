88eaabe7dbccd9fe9abcd4e47d3656128eba327c
Harvey56 <hevel56@gmail.com>
2019-01-07 23:58:48 +1100

    Fixed Queries

--- src/index.tsx
+++ src/index.tsx
@@ -3,35 +3,68 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from './App';
-// import ApolloClient from "apollo-boost";
 import { ApolloProvider } from 'react-apollo';
 
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
+// import { ApolloClient } from 'apollo-client';
+// import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { RestLink } from 'apollo-link-rest';
+// import { RestLink } from 'apollo-link-rest';
+// import gql from 'graphql-tag';
+import ApolloClient, { gql } from 'apollo-boost';
+// import GraphQLClient from './GraphqlClient';
+import { setContext } from 'apollo-link-context';
 
 // setup RestLink with a custom API endpoint
 // const restLink = new RestLink({ uri: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql/" });
 
-import { setContext } from 'apollo-link-context';
 
-const YELP_BASE_URI = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql';
+// const YELP_BASE_URI = 'https://api.yelp.com/v3/graphql';
 
-const httpLink = new HttpLink({
-  uri: YELP_BASE_URI,
-  headers: {
-    authorization: `Bearer ${process.env.API_KEY}`,
-    'content-type': 'application/graphql',
-    'locale': 'en_US'
-  }
-});
+// const httpLink = new HttpLink({
+  // uri: 'http://localhost:4000/graphql'
+  // uri: YELP_BASE_URI,
+  // headers: {
+  //   authorization: "Bearer NXsuVfuXjTiJmlSs1dHn92xnXFpo3G1KaJE0lzQoAOuEMbNyhXkyeWEWoril1xWZU1sMSJQgx1ZAJ0drAup9Sa9FNf5eJCKiWb7_t1eWRhSlkaVuWFXGK2cUjSgaXHYx",
+  //   'content-type': 'application/graphql',
+  //   'locale': 'en_US'
+  // }
+// });
 
-const cache = new InMemoryCache();
+// const cache = new InMemoryCache();
+
+// const defaults = {
+//   isConnected: false,
+
+// }
+
+// const resolvers = {
+//   Mutation: {
+//     updateNetworkStatus: (_, { isConnected }, { cache }) => {
+//       cache.writeData({ data: { isConnected }});
+//       return null;
+//     }
+//   }
+// }
+
+// const stateLink = withClientState({
+//   resolvers,
+//   cache,
+//   defaults
+// });
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache,
+  // link: stateLink,
+  // cache,
+  uri: 'https://server-eizdvlvnq.now.sh/', // 'server-kn4rkamxr.now.sh/',// 'https://event-coordination-app.herokuapp.com' //'http://localhost:4000/graphql',
+  headers: {
+    authorization: "Bearer NXsuVfuXjTiJmlSs1dHn92xnXFpo3G1KaJE0lzQoAOuEMbNyhXkyeWEWoril1xWZU1sMSJQgx1ZAJ0drAup9Sa9FNf5eJCKiWb7_t1eWRhSlkaVuWFXGK2cUjSgaXHYx",
+    'content-type': 'application/json',
+    // 'locale': 'en_US'
+  }
+  // clientState: {
+  //   defaults: defaults,
+  //   resolvers: resolvers
+  // }
 });
 
 
@@ -66,6 +99,21 @@ const client = new ApolloClient({
   // link: httpLink,
   // cache: new InMemoryCache(),
 // })
+
+// client
+//   .query({
+//     query: gql`
+//       query{
+//         businesses(term:"bars", location:"newcastle australia"){
+//           id
+//           name
+//           price
+//           location
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
   
 const renderDom = () => {
   ReactDOM.render(
