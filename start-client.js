06b720d6e3d7db3852733b188174ff34225ae04f
Harvey <hervel56@gmail.com>
2019-04-26 22:00:05 +1000

    Updated CRA start script for build

--- start-client.js
+++ start-client.js
@@ -1,3 +1,3 @@
-const args = [ 'start' ];
+const args = [ 'build' ];
 const opts = { stdio: 'inherit', cwd: 'src/event-coord-app-ui', shell: true };
 require('child_process').spawn('npm', args, opts);
